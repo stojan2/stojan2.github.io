cc010692aab92c74dcb036ac1c81657a5607330a
stojan2 <58376737+stojan2@users.noreply.github.com>
2021-07-27 17:18:05 +0200

    Add files via upload

---
+++ Decision.js
@@ -0,0 +1,23 @@
+function start(){
+  var userAgent = navigator.userAgent.toLowerCase();
+  if(userAgent.match('iphone'))
+    handy();
+  else if(userAgent.match('ipad'))
+    handy();
+  else if(userAgent.match('android'))		
+    handy();
+  else if(userAgent.match('windows phone'))		
+    handy();
+  else
+    pc();
+}
+
+function handy() {
+  document.getElementById('bild1').innerHTML = "<img width=160% src=https://github.com/stojan2/fotos/blob/main/bild2.jpg?raw=true alt=bild ></img>";
+  document.getElementById('bild2').innerHTML = "<img width=160% src=https://github.com/stojan2/fotos/blob/main/bild1.jpg?raw=true alt=bild ></img>";
+  document.getElementById('download').innerHTML = "Download<br> -20MB";
+}
+
+function pc(){
+  
+}
