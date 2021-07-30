16b21df8ebd6d118699bf0c0a6ad95922fd8370a
stojan2 <58376737+stojan2@users.noreply.github.com>
2021-07-30 21:36:37 +0200

    Update Decision.js

--- Decision.js
+++ Decision.js
@@ -14,7 +14,7 @@ function start(){
 
 function handy() {
   document.getElementById('bild1').innerHTML = "<img width=160% src=https://github.com/stojan2/fotos/blob/main/decision1.png?raw=true alt=bild>";
-  document.getElementById('bild2').innerHTML = "<img width=160% src=https://github.com/stojan2/fotos/blob/main/bild1.jpg?raw=true alt=bild ></img>";
+  document.getElementById('bild2').innerHTML = "<img width=160% src=https://github.com/stojan2/fotos/blob/main/decision3.png?raw=true alt=bild ></img>";
   document.getElementById('download').innerHTML = "Download<br> -20MB";
 }
 
