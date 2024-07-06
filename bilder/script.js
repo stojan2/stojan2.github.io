b6082d208f7f19022721908a7e3403a04d1b0db3
stajan2 <58376737+stojan2@users.noreply.github.com>
2024-07-06 18:03:04 +0700

    Add files via upload

---
+++ bilder/script.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    document.getElementById('preloaderBackground').style.display = 'none';
+    document.getElementById('preloaderText').style.display = 'none';
+    document.getElementById('preloader').style.display = 'none';
+}   
