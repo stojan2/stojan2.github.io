f0c71d9ed763e63f6e526bf43f24193437c8dc9f
stajan2 <58376737+stojan2@users.noreply.github.com>
2024-07-06 18:19:37 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
-function check(){
-    var userAgent = navigator.userAgent.toLowerCase();
-    if(userAgent.match('iphone'))
-      noHave();
-    else if(userAgent.match('ipad'))
-      noHave();
-    else if(userAgent.match('android'))		
-      android();
-    else if(userAgent.match('windows phone'))		
-        noHave();
-    else
-      pc();
-}
-
-
-function android() {
-    document.getElementById('pc').innerHTML = "";
-    document.getElementById('noHave').innerHTML = "";
-}
-
-function pc(){
-    document.getElementById('android').innerHTML = "";
-    document.getElementById('noHave').innerHTML = "";
-}
-function noHave(){
-    document.getElementById('android').innerHTML = "";
-    document.getElementById('pc').innerHTML = "";
-}
+window.onload = function () {
+    document.getElementById('preloaderBackground').style.display = 'none';
+    document.getElementById('preloaderText').style.display = 'none';
+    document.getElementById('preloader').style.display = 'none';
+}   
