2f770e8f93b050509e30ae7ddf474dae0a1a1084
stajan2 <58376737+stojan2@users.noreply.github.com>
2024-08-04 23:06:50 +0700

    Add files via upload

--- Lotos Game/script.js
+++ Lotos Game/script.js
@@ -2,4 +2,13 @@ window.onload = function () {
     document.getElementById('preloaderBackground').style.display = 'none';
     document.getElementById('preloaderText').style.display = 'none';
     document.getElementById('preloader').style.display = 'none';
-}   
+
+    document.getElementById('Sot').innerHTML = 
+    `<h1>Контакты:</h1>
+        <center>
+            <a href="https://discord.com/invite/NHJBmkSaSa" target="_blank">
+                <h1 style="color: blueviolet;">Lotos Game</h1>
+            </a>
+        </center>
+    <br>`
+}
