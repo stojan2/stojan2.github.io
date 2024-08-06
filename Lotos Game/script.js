faea6213da73590b630492afa365d3edb739dea0
stajan2 <58376737+stojan2@users.noreply.github.com>
2024-08-06 16:29:44 +0700

    Add files via upload

--- Lotos Game/script.js
+++ Lotos Game/script.js
@@ -3,8 +3,10 @@ window.onload = function () {
     document.getElementById('preloaderText').style.display = 'none';
     document.getElementById('preloader').style.display = 'none';
 
-    if (document.getElementById('Sot_RU')!==null) 
-        document.getElementById('Sot_RU').innerHTML = 
+    var pathname=window.location.pathname;
+
+    if (pathname.substring(pathname.length-9,pathname.length-5)!=="_eng"){
+        document.getElementById('Sot').innerHTML = 
         `<h1>Контакты:</h1>
             <center>
                 <a href="https://discord.com/invite/NHJBmkSaSa" target="_blank">
@@ -12,10 +14,15 @@ window.onload = function () {
                 </a>
             </center>
         <br>`
-
-
-    if(document.getElementById('Sot_ENG')!==null)
-        document.getElementById('Sot_ENG').innerHTML = 
+        document.getElementById("Language").innerHTML = `
+        <center><div class="Languages">
+            <h1 class="Language_Text">Язык:</h1>
+            <a href="${pathname}"><img class="Language_Icon" src="src/ru.png" alt="Russian Language"></a>
+            <a href="${pathname.substring(0,pathname.length-5)}_eng.html"><img class="Language_Icon" src="src/engl.png" alt="English Language"></a>
+        </div></center>`
+    }
+    else{
+        document.getElementById('Sot').innerHTML = 
         `<h1>Contacts:</h1>
             <center>
                 <a href="https://discord.com/invite/NHJBmkSaSa" target="_blank">
@@ -23,4 +30,11 @@ window.onload = function () {
                 </a>
             </center>
         <br>`
+        document.getElementById("Language").innerHTML = `
+        <center><div class="Languages">
+            <h1 class="Language_Text">Language:</h1>
+            <a href="${pathname.substring(0,pathname.length-9)}.html"><img class="Language_Icon" src="src/ru.png" alt="Russian Language"></a>
+            <a href="${pathname}"><img class="Language_Icon" src="src/engl.png" alt="English Language"></a>
+        </div></center>`
+    }
 }
